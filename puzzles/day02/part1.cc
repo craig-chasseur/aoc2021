036a9f8e5d11f339fb00c3032b4289f68a0f9ade
Craig Chasseur <spoonboy42@gmail.com>
2021-12-01 21:09:24 -0800

    Day 2 part 1 solutions

---
+++ puzzles/day02/part1.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "absl/strings/numbers.h"
+#include "absl/strings/str_split.h"
+#include "absl/strings/string_view.h"
+#include "util/check.h"
+#include "util/io.h"
+
+int main(int argc, char** argv) {
+  int depth = 0;
+  int horizontal = 0;
+
+  std::vector<std::string> lines = aoc2021::ReadLinesFromFile(argv[1]);
+  for (const std::string& line : lines) {
+    std::vector<absl::string_view> parts = absl::StrSplit(line, ' ');
+    CHECK(parts.size() == 2);
+    int value = 0;
+    CHECK(absl::SimpleAtoi(parts.back(), &value));
+    if (parts.front() == "forward") {
+      horizontal += value;
+    } else if (parts.front() == "up") {
+      depth -= value;
+    } else if (parts.front() == "down") {
+      depth += value;
+    } else {
+      CHECK_FAIL();
+    }
+  }
+
+  std::cout << (depth * horizontal) << "\n";
+
+  return 0;
+}
