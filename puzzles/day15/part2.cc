290955af063ea967bd40d6ddbdeabd0ffb640363
Craig Chasseur <spoonboy42@gmail.com>
2021-12-14 22:14:26 -0800

    Much faster solution based on A*

--- puzzles/day15/part2.cc
+++ puzzles/day15/part2.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 #include <string>
 #include <vector>
 
@@ -34,36 +35,56 @@ Grid<int> BlowupGrid(const Grid<int>& original_grid) {
   return new_grid;
 }
 
+struct PointAndRisk {
+  PointAndRisk(Point point_in, int risk_in) : point(point_in), risk(risk_in) {}
+
+  Point point;
+  int risk = 0;
+};
+
+struct GreaterRisk {
+  bool operator()(const PointAndRisk& a, const PointAndRisk& b) {
+    return a.risk > b.risk;
+  }
+};
+
+int BestPath(const Grid<int>& risk_grid) {
+  const Point goal{static_cast<int64_t>(risk_grid.XSize()) - 1,
+                   static_cast<int64_t>(risk_grid.YSize()) - 1};
+
+  absl::flat_hash_map<Point, int> path_risks{{Points::kOrigin, 0}};
+  std::priority_queue<PointAndRisk, std::vector<PointAndRisk>, GreaterRisk> point_queue;
+  point_queue.emplace(Points::kOrigin, 0);
+  while (!point_queue.empty()) {
+    PointAndRisk current = point_queue.top();
+    point_queue.pop();
+    if (current.point == goal) return current.risk;
+
+    // It's possible that in the time since a path to this point was put on the
+    // queue, a shorter path to the point was already found and processed first.
+    // Skip re-processing this point if that happened.
+    if (current.risk > path_risks[current.point]) continue;
+
+    for (const Point& adjacent : current.point.AdjacentCardinal()) {
+      if (!risk_grid.InRange(adjacent)) continue;
+      const int tentative_risk = current.risk + risk_grid[adjacent];
+      auto [risk_it, inserted] = path_risks.try_emplace(adjacent, tentative_risk);
+      if (inserted || tentative_risk < risk_it->second) {
+        risk_it->second = tentative_risk;
+        point_queue.emplace(adjacent, tentative_risk);
+      }
+    }
+  }
+
+  CHECK_FAIL();
+}
+
 }  // namespace
 
 int main(int argc, char** argv) {
   std::vector<std::string> lines = aoc2021::ReadLinesFromFile(argv[1]);
-
   Grid<int> risk_grid = BlowupGrid(Grid<int>::ReadFromDigits(lines));
-
-  absl::flat_hash_map<Point, int> risk_to_point{{Points::kOrigin, 0}};
-  absl::flat_hash_set<Point> new_points{Points::kOrigin};
-  while (!new_points.empty()) {
-    absl::flat_hash_set<Point> next_points;
-    for (const Point& point : new_points) {
-      for (const Point& adjacent : point.AdjacentCardinal()) {
-        if (!risk_grid.InRange(adjacent)) continue;
-        const int risk = risk_to_point[point] + risk_grid[adjacent];
-        auto [risk_it, inserted] = risk_to_point.try_emplace(adjacent, risk);
-        if (inserted || risk_it->second > risk) {
-          risk_it->second = risk;
-          next_points.emplace(adjacent);
-        }
-      }
-    }
-    new_points = std::move(next_points);
-  }
-
-  auto destination_it =
-      risk_to_point.find({static_cast<int64_t>(risk_grid.XSize()) - 1,
-                          static_cast<int64_t>(risk_grid.YSize()) - 1});
-  CHECK(destination_it != risk_to_point.end());
-  std::cout << destination_it->second << "\n";
+  std::cout << BestPath(risk_grid) << "\n";
 
   return 0;
 }
